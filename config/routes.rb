fd17f7de4ab87e32582b7fcdcc64a0c7eafb7b4a
HirokiMiyakura <miyakurahiroki@gmail.com>
2021-05-10 21:50:13 +0900

    Delete unnecessary controllers and fix translations of _header.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_for :users
   resources :users, only: [:index, :show]
   resources :books
 end
